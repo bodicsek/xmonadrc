142e991a59520171c7205ab56251e7f2ebe5de51
David Nabraczky <david.nabraczky@gmail.com>
2015-05-15 15:19:21 +0200

    adjusted volume.sh

--- volume/volume.sh
+++ volume/volume.sh
@@ -6,7 +6,7 @@ if [ $mute_state -eq 1 ]
 then
     vol="--"
 else
-    vol=`pactl list sinks | grep 'Volume: 0:'| sed -r 's/.+Volume: 0:\s*([0-9]+)%.+/\1/'`
+    vol=`pactl list sinks | grep 'Volume: front-left:' | sed -r 's/Volume: front-left:.+\/\s*([0-9]+)%.+/\1/'`
 fi
 
 echo "Vol:" $vol
